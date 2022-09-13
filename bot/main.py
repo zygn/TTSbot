0d7ce97d4fdd615aed68c4cf08c1f72ebc43ff85
YundoChoi <rikochet@kakao.com>
2022-09-13 18:41:24 +0900

    fixed re-summoning method

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from collections import deque
 import os
 import nextcord
@@ -388,6 +389,14 @@ class BotCommands(commands.Cog):
             log.error("No voice channel found. Join the voice channel and try again.")
             await channel.send("No voice channel found. Join the voice channel and try again.")
             return
+
+        except nextcord.errors.ApplicationInvokeError:
+            log.debug("Try to disconnect and re-summoning")
+            voice_client = voice_client.disconnect(force=True)
+
+            await asyncio.sleep(0.05)
+            destination = author.voice.channel
+            voice_client = await destination.connect()
 
         await interaction.response.send_message(f"음성채널 <#{destination.id}> 으로 소환되었습니다.")
         return
